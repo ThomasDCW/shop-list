63c7ee7d214cce31cc0c14e0de17d9e6d91f70f0
Thomas Di Cola <thomas.dicola@user-agency.com>
2025-06-08 10:31:04 +0200

    pwa

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { InstallPWA } from "./components/InstallPWA";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <InstallPWA />
+    <div className="min-h-screen p-8">
+      <main className="max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Shop List</h1>
       </main>
     </div>
   );
