e3812ad5ee60bccc0da6d07567de6e65e5c7de18
Thomas Di Cola <thomas.dicola@user-agency.com>
2025-06-08 11:36:24 +0200

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import icon from '@/app/assets/pwa-icon.png';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="min-h-screen p-8">
-      <main className="mx-auto max-w-xl">
-        <h1 className="mb-6 text-3xl font-bold">Shop List</h1>
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+      <main className="mx-auto max-w-xl px-4 py-4 sm:px-6 lg:px-8">
+        <div className="flex items-center gap-2">
+          <Image className="w-[32px]" src={icon} alt="Shop List" priority />
+          <h1 className="font-bold tracking-tight">
+            <span className="bg-gradient-to-r from-[#ff761e] to-[#ff9500] bg-clip-text text-transparent">
+              Shop List
+            </span>
+          </h1>
+        </div>
       </main>
     </div>
   );
